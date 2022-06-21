829f23588584f524c0c8d9ed45533cdf4e2b3c62
Murali I <indukurimkv@gmail.com>
2022-06-21 12:12:49 -0500

    Add compute graph

--- Program/tree.py
+++ Program/tree.py
@@ -1,7 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 21 10:54:11 2022
+import numpy as np
 
-@author: induk
-"""
+class Node:
+    lookup = {}
+    @staticmethod
+    def register(key,val):
+        Node.lookup[key] = val
+    def __init__(self, func = lambda a,b: a+b, ids=None):
+        self.func = func
+        self.children = ids
+        
+    def evaluate(self):
+        args = []
+        for child in self.children:
+            if not isinstance(child, Node):
+                args.append(Node.lookup[child])
+            else:
+                args.append(child.evaluate())
+                
+        return self.func(*args)
+    
 
+if __name__ == "__main__":
+    pass
